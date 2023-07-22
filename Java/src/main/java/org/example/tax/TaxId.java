0e5cd1ff9bfcef0198d6b7f6d8e02b81c7e13eca
Raimund Kraemer <kraemer_raimund@web.de>
2023-07-22 12:22:32 +0200

    Simplify tax ID validation using regex
    Often regular expressions can be quite hard to read, but in some cases
    a very simple regex can be easy to understand and at the same time make
    the implementation of validation rules much simpler, like in this case.
    And if the tests are already there, we can safely refactor the
    implementation.

--- Java/src/main/java/org/example/tax/TaxId.java
+++ Java/src/main/java/org/example/tax/TaxId.java
@@ -3,18 +3,8 @@ package org.example.tax;
 final class TaxId {
 
     public static TaxId parse(String internalValue) throws IllFormedTaxIdException {
-        if (internalValue.isEmpty()) {
+        if (!internalValue.matches("[0-9]{11}")) {
             throw new IllFormedTaxIdException(internalValue);
-        }
-
-        if (internalValue.length() != 11) {
-            throw new IllFormedTaxIdException(internalValue);
-        }
-
-        for (final var character : internalValue.toCharArray()) {
-            if (!Character.isDigit(character)) {
-                throw new IllFormedTaxIdException(internalValue);
-            }
         }
 
         return null;
